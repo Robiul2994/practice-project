ab883db2fdd515ec127b3322dc57d03f390346dc
Robiul2994 <rabiul15-2994@diu.edu.bd>
2022-04-24 23:09:17 +0600

    banner true

--- 11-4-22/evana/lib/main.dart
+++ 11-4-22/evana/lib/main.dart
@@ -11,7 +11,7 @@ class my_apps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Center(
         child: Scaffold(
           appBar: AppBar(
